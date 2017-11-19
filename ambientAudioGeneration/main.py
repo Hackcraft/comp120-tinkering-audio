d23551ac35514b6470db214588b0cf6bc88a4eac
Hackcraft <zakary@zakary.org>
2017-11-19 15:41:26 +0000

    Working buttons.

--- ambientAudioGeneration/main.py
+++ ambientAudioGeneration/main.py
@@ -37,11 +37,10 @@ screen = pygame.display.set_mode((screenX, screenY))
 # Button text
 buttonTextSize = screenX / len(ambience) / 2
 buttonTextFont = pygame.font.SysFont("monospace", buttonTextSize)
-label = buttonTextFont.render("Some text!", 1, (0,0,0))
 
 buttonText = []
-for place in ambience:
-    buttonText.append(buttonTextFont.render(place, 1, (0,0,0)))
+for place in sorted(ambience.items()): # Go through the dictionary in alphabetical order
+    buttonText.append(buttonTextFont.render(place[0], 1, (0,0,0)))
 
 blnRunning = True
 while blnRunning:
@@ -61,3 +60,12 @@ while blnRunning:
         if event.type == pygame.QUIT:
             blnRunning = False
             pygame.quit()
+        # Button actions
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            mouseX, mouseY = pygame.mouse.get_pos()
+            if mouseY < buttonTextSize:
+                print sorted(ambience.items())[0][0] # Caves
+            elif mouseY < buttonTextSize * 2:
+                print sorted(ambience.items())[1][0] # Forests
+            elif mouseY < buttonTextSize * 3:
+                print sorted(ambience.items())[2][0] # Towns

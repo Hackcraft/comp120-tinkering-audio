38eaaf1c325425b59e7c0de2483abb742820a491
Hackcraft <zakary@zakary.org>
2017-11-19 15:22:58 +0000

    Gui base.

--- ambientAudioGeneration/main.py
+++ ambientAudioGeneration/main.py
@@ -1,3 +1,5 @@
+import math, pygame
+
 """
     ambience contains the different ambient noise scenarios,
     their sound files and the probability of them playing.
@@ -23,3 +25,18 @@ ambience = {
 
     ]
 }
+
+"""
+    Gui
+"""
+
+pygame.init()
+screenX, screenY = 800, 600
+screen = pygame.display.set_mode((screenX, screenY))
+
+blnRunning = True
+while blnRunning:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            blnRunning = False
+            pygame.quit()

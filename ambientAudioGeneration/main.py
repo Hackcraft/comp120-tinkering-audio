03c9b1d6ef6adfcaacd25c3ed0ab39e6053c3843
Hackcraft <zakary@zakary.org>
2017-11-19 15:17:10 +0000

    Prototype structure.

---
+++ ambientAudioGeneration/main.py
@@ -0,0 +1,25 @@
+"""
+    ambience contains the different ambient noise scenarios,
+    their sound files and the probability of them playing.
+"""
+
+ambience = {
+    "Forests" : [
+        # Sound one
+        [
+            # Load file
+            # Probability of it playing
+        ],
+        # Sound two
+        [
+            # Load file
+            # Probability of it playing
+        ],
+    ],
+    "Towns" : [
+
+    ],
+    "Caves" : [
+
+    ]
+}

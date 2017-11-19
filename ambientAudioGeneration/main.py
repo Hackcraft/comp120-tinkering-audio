cbe0123c3d96fddbd5f993bce1c939c84ab16b8a
Hackcraft <zakary@zakary.org>
2017-11-19 15:33:48 +0000

    Buttons without click.

--- ambientAudioGeneration/main.py
+++ ambientAudioGeneration/main.py
@@ -34,8 +34,29 @@ pygame.init()
 screenX, screenY = 800, 600
 screen = pygame.display.set_mode((screenX, screenY))
 
+# Button text
+buttonTextSize = screenX / len(ambience) / 2
+buttonTextFont = pygame.font.SysFont("monospace", buttonTextSize)
+label = buttonTextFont.render("Some text!", 1, (0,0,0))
+
+buttonText = []
+for place in ambience:
+    buttonText.append(buttonTextFont.render(place, 1, (0,0,0)))
+
 blnRunning = True
 while blnRunning:
+
+    # Plain white screen
+    screen.fill((255, 255, 255))
+
+    # Button text
+    for i in xrange(len(buttonText)):
+        screen.blit(buttonText[i], (0, i * buttonTextSize))
+
+    # Update screen
+    pygame.display.update()
+
+    # Allow the user to quit
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             blnRunning = False

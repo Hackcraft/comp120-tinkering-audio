61f059a4cdf14fd3e440f037e3aa078497420e23
Hackcraft <zakary@zakary.org>
2017-11-28 14:33:18 +0000

    Changed var names.

--- ambientAudioGeneration/soundFunctions.py
+++ ambientAudioGeneration/soundFunctions.py
@@ -7,15 +7,15 @@ class SoundFunctions:
     def __init__(self):
         print "Loaded SoundFunctions"
 
-    def combineTones(self, toneOne, toneTwo, sampleLength):
-        sampleLength = sampleLength.variables.sampleLength
+    def combineTones(self, toneOne, toneTwo, soundData):
+        sampleLength = soundData.variables.sampleLength
         values = []
         for i in range(0, sampleLength):
             values.append(toneOne[i] + toneTwo[i])
         return values
 
-    def makeChord(self, sound1, sound2, sound3, sampleLength):
-        sampleLength = sampleLength.variables.sampleLength
+    def makeChord(self, sound1, sound2, sound3, soundData):
+        sampleLength = soundData.variables.sampleLength
         values = []
         for i in range(0, sampleLength):
             values.append(sound1[i])
@@ -26,8 +26,8 @@ class SoundFunctions:
 
         return values
 
-    def echo(self, sound1, sound2, delay, sampleLength):
-        sampleLength = sampleLength.variables.sampleLength
+    def echo(self, sound1, sound2, delay, soundData):
+        sampleLength = soundData.variables.sampleLength
         values = []
         for i in range(0, sampleLength):
             values.append(sound1[i])
@@ -36,14 +36,14 @@ class SoundFunctions:
                 values.append(echo + sound1[i])
         return values
 
-    def saveWaveFile(self, filename, wavData, sound):
+    def saveWaveFile(self, filename, wavData, soundData):
         packedValues = []
         for i in range(0, len(wavData)):
             packedValue = struct.pack('h', wavData[i])
             packedValues.append(packedValue)
 
         noiseOut = wave.open(filename, 'w')
-        noiseOut.setparams((sound.CHANNELS, sound.SAMPLE_WIDTH, sound.SAMPLE_RATE, 0, 'NONE', 'not compressed'))
+        noiseOut.setparams((soundData.CHANNELS, soundData.SAMPLE_WIDTH, soundData.SAMPLE_RATE, 0, 'NONE', 'not compressed'))
         valueStr = ''.join((str(n) for n in packedValues))
         noiseOut.writeframes(valueStr)
         noiseOut.close()
